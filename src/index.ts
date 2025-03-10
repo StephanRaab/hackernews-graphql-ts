3772868ecc91df88eb6fd052f658a4565379670f
Stephan Raab <sraab@idsfulfillment.com>
2025-03-10 14:55:16 -0400

    use playground

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import {ApolloServer} from "apollo-server";
+import {ApolloServer} from "apollo-server';
+import {ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core";
 
 import {schema} from "./schema";
-export const server = new ApolloServer({schema});
 
+export const server = new ApolloServer({
+	schema,
+	plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+})
 const port = 3000;
 server.listen({port}).then(({url}) => {
 	console.log(`🚀 server ready at ${url}`);
